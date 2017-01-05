24d2a274ff9bf4697c7c2f8c5f34b12fb3392059
Drew Schuster <dtschust@gmail.com>
2017-01-04 18:43:29 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    return mod(require("../lib/infer"), require("../lib/tern"), require("acorn/dist/walk"))
+  if (typeof define == "function" && define.amd) // AMD
+    return define(["../lib/infer", "../lib/tern", "acorn/dist/walk"], mod)
+  mod(tern, tern, acorn.walk)
+})(function(infer, tern, walk) {
+  "use strict"
+
+  function findModules(file) {
+
+    walk.simple(file.ast, {
+      CallExpression: function(node) {
+        var callee = node.callee
+        if (callee && callee.object && callee.object.name === 'TS' && callee.property && callee.property.name && callee.property.name === 'registerModule') {
+          //get top scope:
+          var scope = file.scope
+          while (scope.prev) {
+            scope = scope.prev
+          }
+
+          if (scope.name !== '<top>') {
+            console.log('error!')
+          }
+
+          // Create a global TS if it doesn't exist
+          if (!scope.hasProp('TS')) {
+            var outObj = new infer.Obj(true)
+            scope.defProp('TS', outObj);
+            scope.props.TS.addType(outObj);
+          }
+          var TS = scope.props.TS;
+          var moduleName = node.arguments[0].value;
+          var depth = TS.types[0];
+          if (moduleName.split('.').length > 1) {
+            var scopes = moduleName.split('.').slice(0, -1);
+            moduleName = moduleName.split('.').slice(-1).join();
+            var scope = scopes.shift();
+            while (scope) {
+              debugger;
+              if (depth.hasProp(scope)) {
+                // do nothing, we're golden
+              } else {
+                var newObj = new infer.Obj(true)
+                depth.defProp(scope, newObj)
+                depth.props[scope].addType(newObj)
+              }
+              depth = depth.props[scope].types[0]
+              scope = scopes.shift();
+            }
+            debugger;
+          }
+          var module = node.arguments[1];
+          depth.defProp(moduleName, module);
+          depth.props[moduleName].addType(module.objType);
+        }
+      },
+    })
+  }
+
+  tern.registerPlugin("es_modules", function(server) {
+    server.on("afterLoad", findModules)
+  })
+})
+
